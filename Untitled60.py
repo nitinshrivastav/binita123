1451b4aef5d5c0e472720b726a06cbfed432f07c
Nitin Shrivastav <38551867+nitinshrivastav@users.noreply.github.com>
2021-04-11 14:32:20 +0530

    Add files via upload

---
+++ Untitled60.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route("/")
+def xyz():
+    return render_template("sum.html")
+@app.route("/su",methods=['GET','POST'])
+
+def dfg():
+    if(request.method=='POST'):
+        num1=int(request.form['a'])
+        num2=int(request.form['b'])
+        total=num1+num2
+        
+        return  render_template('sum.html',s=total)
+if __name__=='__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
